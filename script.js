2ccfe1ab529e138e8d07a04e2dfeff4c444e0c53
shivani joshi <161042809+749977@users.noreply.github.com>
2025-03-07 15:43:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+document.getElementById("removeButton").addEventListener("click", function() {
+    let select = document.getElementById("colorSelect");
+    select.remove(select.selectedIndex);
+});
+
